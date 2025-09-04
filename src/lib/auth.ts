71910804c5f10898a5ac3a29709d161bebd99c72
Manish Mahto <manishrocki2000@gmail.com>
2025-09-04 07:50:42 +0530

    default secret key add

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 import { NextRequest } from 'next/server';
 
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SECRET = process.env.JWT_SECRET || 'secretkey';
 
 
 export interface JwtPayload {
